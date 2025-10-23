9a878b08c5e551500627a315fbac09403304b88e
SeanMatthews26 <seanmatthews2696@gmail.com>
2025-10-23 15:58:10 +0100

    created review model

--- blog/models.py
+++ blog/models.py
@@ -1,3 +1,22 @@
 from django.db import models
+from django.core.validators import MinValueValidator, MaxValueValidator
+from django.contrib.auth.models import User
+
+STATUS = ((0, "Draft"), (1, "Published"))
 
 # Create your models here.
+
+
+class Review(models.Model):
+    title = models.CharField(max_length=200, unique=True)
+    slug = models.SlugField(max_length=200, unique=True)
+    author = models.ForeignKey(
+        User, on_delete=models.CASCADE, related_name="reviews"
+    )
+    content = models.TextField()
+    created_on = models.DateTimeField(auto_now_add=True)
+    status = models.IntegerField(choices=STATUS, default=0)
+    rating = models.IntegerField(
+        validators=[MinValueValidator(0), MaxValueValidator(10)],
+        default=0,
+    )
