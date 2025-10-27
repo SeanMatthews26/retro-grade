6ac655a4d8c17dc9b1c3855a8e8fc99165fe38fc
SeanMatthews26 <seanmatthews2696@gmail.com>
2025-10-27 15:19:55 +0000

    generic view

--- blog/views.py
+++ blog/views.py
@@ -1,6 +1,9 @@
 from django.shortcuts import render
-from django.http import HttpResponse
+from django.views import generic
+from .models import Review
+
 
 # Create your views here.
-def my_blog(request):
-    return HttpResponse("Hello, blog!")
+class ReviewList(generic.ListView):
+    queryset = Review.objects.all()
+    template_name = "review_list.html"
